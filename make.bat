401467214160484e04485fc7e4ea983901bf133a
Klanly <natural858@gmail.com>
2025-03-20 18:44:06 +0800

    Update make.bat

--- make.bat
+++ make.bat
@@ -128,19 +128,13 @@ if "%TEST%" == "1" (
 	goto EOF
 )
 
-if "%BUILD_WITH_NINJA%" == "" (
-	call "%BLENDER_DIR%\build_files\windows\configure_msbuild.cmd"
-	if errorlevel 1 goto EOF
 
-	call "%BLENDER_DIR%\build_files\windows\build_msbuild.cmd"
-	if errorlevel 1 goto EOF
-) else (
 	call "%BLENDER_DIR%\build_files\windows\configure_ninja.cmd"
 	if errorlevel 1 goto EOF
 
 	call "%BLENDER_DIR%\build_files\windows\build_ninja.cmd"
 	if errorlevel 1 goto EOF
-)
+
 
 :EOF
 if errorlevel 1 exit /b %errorlevel%
